91fdf640108519db1d31d7555d3bd4f885843efd
jagitdev <javibergasapalacios@gmail.com>
2022-04-07 20:36:42 +0200

    Actualizar archivos 1

--- Ejercicio_4.sh
+++ Ejercicio_4.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 
+nombreArchivo='Javier_Bergasa.txt'
+
+if [ -d $nombreArchivo ]
+then
+	echo "Existe el archivo $nombreArcivo"
+	
+else
+	echo "NO Existe el archivo $nombreArchivo"
+	touch $nombreArchivo
+fi
+
+
